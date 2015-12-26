27595913c704e003be609198594f30dc2f0b3730
lauryndbrown <lauryndbrown@gmail.com>
2015-12-25 21:25:44 -0600

    Got the views working

--- fit/views.py
+++ fit/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    context = []
+    return render(request, 'index.html', context)
+
+def about(request):
+    context = []
+    return render(request, 'about.html', context)
+
+def game(request):
+    context = []
+    return render(request, 'game.html', context)
